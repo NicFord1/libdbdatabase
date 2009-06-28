3f3fec4e0e3d6da146be89748825e717ef7a3748
ikool87 <ikool87@c7fa5d22-51dc-11de-9a16-8934cb1294a2>
2009-06-28 22:17:35 +0000

    Add plugins directory which contains one plugin class for each media type and supports (as of now) the following methods:
    search($query, $database)

---
+++ PHP/plugins/book.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once("include/pluginUtils.php");
+
+require_once("include/database.php");
+//require_once("include/db-config.php");
+//require_once("include/session.php");
+
+addPlugin(new BookPlugin());
+
+class BookPlugin
+{
+  public function search($query, $database)
+  {
+   $q = "SELECT * FROM ldb_books WHERE author LIKE \"%$query%\" OR "
+       ."description LIKE \"%$query%\" OR title LIKE \"%$query%\""
+       ." ORDER BY author";
+
+   $results = mysql_query($q);
+   if($results) {
+ 	   $numresults = mysql_num_rows($results);
+   }
+
+   if ($numresults == 0) {
+//      echo "<p>Sorry, your search: &quot;".$trimmed."&quot; returned zero results</p>";
+   } else {
+	   if(empty($s)) {
+	      $s = 0;
+	   }
+
+	   // get results
+//           $q .= " limit $s,$limit";
+	   $result = $database->query($q);
+
+	   // begin to show results set
+	   $count = 1 + $s ;
+		echo "<br /><table border='1'><tr>";
+
+		$itemFields = array("ISBN", "Title", "Author", "Publisher", "ReleaseDate", "Rating", "Description");
+//                for($i=0; $i<count($itemFields); $i++)
+//                {
+//                    //$field = mysql_fetch_field($result);
+//                    echo "<td>$itemFields[$i]</td>";
+//                }
+
+	   while ($row = mysql_fetch_array($result)) {
+             $resultslist[$index] = $row;
+//                        echo "<tr>";
+//                        echo "<td>".$row["isbn"]."</td>";
+ //                       echo "<td>".$row["title"]."</td>";
+//                        echo "<td>".$row["author"]."</td>";
+//                        echo "<td>".$row["publisher"]."</td>";
+//                        echo "<td>".$row["releasedate"]."</td>";
+//                        echo "<td>".$row["rating"]."</td>";
+//                        echo "<td>".$row["description"]."</td>";
+//                        echo "</tr>";
+	      $count++ ;
+              $index++;
+	   }
+//           echo "</table><br />";
+
+            return $resultslist;
+   } // if
+  } // search
+} // class
+
+?>
