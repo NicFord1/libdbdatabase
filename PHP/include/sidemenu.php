b67f31f00c3963074377d6d0234dcff6f0755ee4
Nicholas Ford <Code+NicFord1@Nicks-Net.biz>
2009-07-01 15:01:45 +0000

    changed around the side menu to be more "substantial"....nothing big....

--- PHP/include/sidemenu.php
+++ PHP/include/sidemenu.php
@@ -6,22 +6,28 @@
 if (strpos(strtolower($_SERVER['PHP_SELF']), 'sidemenu.php') !== false) {
    header("Location: ".SITE_BASE_URL."/index.php"); //Gracefully leave page
 }
+
+/**
+ * Display general main menu.
+ */
 //Tie in Main menu with media type plugin system if possible/doable by demo time...
-?>
-      <a id="sectionmenu"></a>
-      <h1>Main menu:</h1>
-      <p class="menublock">
-       <a class="nav" href="#">Browse By:</a><br class="hide" />
-       <a class="nav sub" href="<?php echo SITE_BASE_URL; ?>/index.php?type=BOOK">Books</a><br class="hide" />
-       <a class="nav sub" href="<?php echo SITE_BASE_URL; ?>/index.php?type=CD">CDs</a><br class="hide" />
-       <a class="nav sub" href="<?php echo SITE_BASE_URL; ?>/index.php?type=DVD">DVDs</a><br class="hide" />
-       <a class="nav sub" href="<?php echo SITE_BASE_URL; ?>/index.php?type=PERIODICAL">Periodicals</a><br class="hide" />
-       <a class="nav" href="<?php echo SITE_BASE_URL; ?>/search.php">Search</a><br class="hide" />
-       <a class="hide" href="#top">Back to top</a>
-      </p>
+echo "<a id=\"sectionmenu\"></a>\n";
+echo "<h1>Main menu:</h1>\n";
+echo "<p class=\"menublock\">\n";
+echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/index.php\">Browse</a><br class=\"hide\" />";
+echo "<a class=\"nav sub\" href=\"".SITE_BASE_URL."/index.php?type=BOOK\">Books</a><br class=\"hide\" />\n";
+echo "<a class=\"nav sub\" href=\"".SITE_BASE_URL."/index.php?type=CD\">CDs</a><br class=\"hide\" />\n";
+echo "<a class=\"nav sub\" href=\"".SITE_BASE_URL."/index.php?type=DVD\">DVDs</a><br class=\"hide\" />\n";
+echo "<a class=\"nav sub\" href=\"".SITE_BASE_URL."/index.php?type=PERIODICAL\">Periodicals</a><br class=\"hide\" />\n";
+echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/search.php\">Search</a><br class=\"hide\" />\n";
 
+if(!$session->logged_in) {
+	echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/newuser.php\">Register</a><br class=\"hide\" />\n";
+}
 
-<?php
+echo "<a class=\"hide\" href=\"#top\">Back to top</a>\n";
+echo "</p>\n";
+
 /**
  * Display relavent menus based on user privileges.
  */
@@ -34,11 +40,19 @@ if($session->isTeller() || $session->isAdmin()) { //Teller or Admin
    echo "<p class=\"menublock\">\n";
    echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/checkout.php\">Check Out</a><br class=\"hide\" />\n";
    echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/checkin.php\">Check In</a><br class=\"hide\" />\n";
+   if($session->isTeller()) {
+      echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/newuser.php\">Register New Member</a><br class=\"hide\" />\n";
+   }
    echo "<a class=\"hide\" href=\"#top\">Back to top</a>\n";
    echo "</p>\n";
 }
 
 if($session->isAdmin()) { //Admin
    //should anything go here in an administrator menu?
+   echo "<h1>Administrator menu:</h1>\n";
+   echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/admin/admin.php\">Admin Center</a><br class=\"hide\" />\n";
+   echo "<a class=\"nav\" href=\"".SITE_BASE_URL."/newuser.php\">Register New Member</a><br class=\"hide\" />\n";
+   echo "<a class=\"hide\" href=\"#top\">Back to top</a>\n";
+   echo "</p>\n";
 }
 ?>
