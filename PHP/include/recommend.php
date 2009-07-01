4566f05dc8b4bfafb15d50d45c161416cd28170e
dbergman3 <dbergman3@c7fa5d22-51dc-11de-9a16-8934cb1294a2>
2009-07-01 14:31:59 +0000

    Has the reccomend(itemid) function in it that reccommends another item based on the input item.

---
+++ PHP/include/recommend.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * recommend.php
+ *
+ * This class has function(s) that recommend items to users based on
+ * database history.
+ */
+
+if (strpos(strtolower($_SERVER['PHP_SELF']), 'recommend.php') !== false) {
+   header("Location: ".SITE_BASE_URL."/index.php"); //Gracefully leave page
+}
+
+require_once("database.php");
+
+class Recommend{
+	
+	/* Given an itemid that identifies an item, the function will return
+	 * the item most borrowed by users who borrowed the input item
+	 * (other than the input item itself) */
+	public static function recommend($itemid){
+		
+	}
+};
+?>
