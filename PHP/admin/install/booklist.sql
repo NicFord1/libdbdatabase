88724e260e56b34d991b2d71afe5df40a548c5d7
Nicholas@Nicks-Net.us <Nicholas@Nicks-Net.us@c7fa5d22-51dc-11de-9a16-8934cb1294a2>
2009-07-03 13:51:44 +0000

    Awesome SQL for importing some books into the database....the cool thing is that it inserts a new item and uses the correct itemid on insertion of the book.... ^_^

---
+++ PHP/admin/install/booklist.sql
@@ -0,0 +1,178 @@
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'7934250380',
+'Ethernet: The Definitive Guide',
+'Charles E. Spurgcon',
+'networking',
+'O''Reilly',
+02202000,
+3,
+'This is a book about Ethernet, a local area network (LAN) technology that allows you to connect a variety of computers together with a low-cost and extremely flexible network system.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'5075487481',
+'The Electrical Engineering Handbook',
+'Richard C. Dorf',
+'electrical engineering',
+'CHAPMAN and HALL/CRCnetBase',
+02022000,
+3,
+'A CRC handbook on the subject of Electrical Engineering',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'715729540',
+'Calculus of Finite Differences',
+'Charles Jordan',
+'mathematics',
+'Chelsea Publishing Company',
+01011950,
+3,
+'There is more than mere coincidence in the fact that the recent rapid growth in the theory and application of mathematical statistics has been accompanied by a revival in interest in the Calculus of Finite Differences.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'2418868465',
+'Adobe Premiere 6 Bible',
+'Adele Droblas and Seth Greenberg',
+'graphic arts',
+'Hungry Minds',
+01012001,
+3,
+'This book shows the new features of Adobe Premier 6 and how to use those features.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'5480892867',
+'Advanced Number Theory',
+'Harvey Cohn',
+'mathematics',
+'Dover Publications',
+01011962,
+3,
+'This book goes over the concepts of advanced number theory.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'7267290758',
+'OpenGL Programming Guide',
+'Silicon Graphics',
+'programming',
+'Addison Wesley Longman',
+01011997,
+3,
+'The OpenGL graphics system is a software interface to graphics hardware.  It allows you to create interactive programs that produce color images of moving three-dimensional objects.  With OpenGL, you can control computer-graphics technology to produce realistic pictures or ones that depart from reality in imaginative ways.  This guide explains how to program with the OpenGL graphics system to deliver the visual effect you want.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'5022317998',
+'Teach Yourself Visual C++ 6 in 21 Days',
+'Davis Chapman',
+'programming',
+'SAMS',
+02051998,
+3,'Welcome to Visual C++. Over the next 21 days, you will learn how to use the features that Microsoft has built into its C++ development environment to enable you to create very advanced applications for the Windows and NT platforms.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'8506176444',
+'Mastering PHP 4.1',
+'Jeremy Allen and Charles Hornberger',
+'programming',
+'Sybex',
+04212002,
+3,
+'Developing applications and sites for the World Wide Web, or for Web-like uses such as intranets, has become one of the most extensive areas of computing and programming work. If it can be done digitally, then somebody, somewhere, is trying to adapt it to a web browser. Understanding the various flavors of web activity? Static and dynamic pages, client-side and server-side systems? Is a necessary step toward increasing your flexibility as a developer. PHP builds upon the familiar structure of programming languages such as C, Java, and Perl. It helps create dynamic HTML content by providing the necessary tools to easily manipulate that content. PHP is',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'5602867340',
+'3ds max 4 Bible',
+'Kelly L. Murdock',
+'graphic arts',
+'Hungry Minds',
+05092001,
+3,
+'Let me paint a picture of the writing process. It starts with years of experience, which is followed by months of painstaking research. There were system crashes and personal catastrophes and the always present, ever-looming deadlines. I wrote into the early hours of the morning and during the late hours of the night? Burning the candle at both ends and in the middle all at the same time. It was grueling and difficult, and spending all this time staring at the Max interface made me feel like...well...like an animator.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'7715439068',
+'JAVA IN A NUTSHELL',
+'David Flanagan',
+'programming',
+'O''Reilly',
+05121997,
+3,
+'This book teaches one how to use Java 1.1 and the new features presented in this version of Java.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'9994359770',
+'Java Cryptography',
+'Jonathan Knudsen',
+'programming',
+'O''Reilly',
+05201998,
+3,
+'This book is written for moderately experiences Java developers who are interested in cryptography.  It describes cryptographics development in Java.',
+20.0,
+0.5
+);
+
+INSERT INTO `ldb_items` VALUES(NULL, 'BOOK', 5);
+INSERT INTO `ldb_books` VALUES(
+LAST_INSERT_ID(),
+'855377705',
+'SSH, the Secure Shell: The Definitive Guide',
+'Daneil J. Barrett and Richard E. Silverman',
+'networking',
+'O''Reilly',
+02202001,
+3,
+'SSH is a low-cost, software-based solution for keeping prying eyes away from the data on a network.  It doesn''t solve every privacy and security problem, but it eliminates several of them effectively.  This book shows you how to use SSH to secure the data on your network.',
+20.0,
+0.5
+);
